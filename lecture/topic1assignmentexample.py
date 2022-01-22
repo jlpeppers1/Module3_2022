097a419581b898e2097cda2182e2366833a4285a
Joe <jlpeppers1@dmacc.edu>
2022-01-22 15:48:47 -0600

    adding rough pseudocode

---
+++ lecture/topic1assignmentexample.py
@@ -0,0 +1,24 @@
+"""
+In the shell or in a text editor, write a program that asks for the user to sign up for Programmer's Toolkit Monthly Subscription Box. They must select level of membership they want. Each month is something different, t-shirts, stickers, figurines, even programming books!
+The levels are the following:
+    Platinum
+    Gold
+    Silver
+    Bronze
+    Free Trial
+Write an if statement that prints the cost for each of the level. Platinum is $60, each level below is 10 dollars cheaper, and the free trial is free.
+Submit your .py file.
+"""
+
+#Print some details to the screen
+    #talk about the Prog Toolkit Month
+    #talk about what it could include
+    #list out the levels
+    #please select a level to hear the price
+#Get some sort of input from the user
+
+#figure which level the user selected; the hw wants us to use an if statement
+    #if the user selected platinum then print 'the cost is $60'
+    #if the user selected gold then print 'the cost is $10'
+    #...
+print('placeholder')
