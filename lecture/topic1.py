99cb9166ea1d04abe54224e0ece59f479b654dbc
Joe <jlpeppers1@dmacc.edu>
2022-01-22 15:32:48 -0600

    adding topic 1 examples

---
+++ lecture/topic1.py
@@ -0,0 +1,14 @@
+#Sequential
+print('This is sequential')
+conditional = False
+#conditional
+if (conditional):
+    #statements below here
+    print('We are in the if branch') #if branch
+elif(not conditional):
+    print('We are in the else if')
+else:
+    print('We are in the else branch') #else branch
+#Back to sequential
+print('This always happens since it is not indented')
+
